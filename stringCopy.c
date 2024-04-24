de8c461539b6330e09bc5c538090b008c0203025
asus test <msa-99@outlook.com>
2024-04-24 12:23:20 +0600

    string copy on another

--- stringCopy.c
+++ stringCopy.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 #include<limits.h>
+#include<string.h>
 int main(){
-      
+    char a[100], b[100];
+    scanf("%s %s",&a,&b);
+    // printf("%d",strlen(b)); //6 without null \ \o
+    // printf("%s %s",a,b);
+    for(int i=0; i<=strlen(b); i++){
+        a[i]=b[i];
+    }
+    printf("%s %s",a,b);
     return 0;
 }
+//input: apple orange
+//output: orangea orange
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,5 @@
+/ printf("%s %s",a,b);
+    // for(int i=0; i<strlen(b); i++){
+    //     a[i]=b[i];
+    // }
+    // printf("%s %s",a,b);
