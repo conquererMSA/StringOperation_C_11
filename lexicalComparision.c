0fdae6f92383057ae9d724d2e6676a05a8c089a0
asus test <msa-99@outlook.com>
2024-04-25 13:19:22 +0600

    lexicographical comparision

--- lexicalComparision.c
+++ lexicalComparision.c
@@ -6,12 +6,12 @@ int main(){
     scanf("%s %s",a,b);
     int i=0;
     while(1){
-        //null checking
-        if(a[i]=='\0'&& b[i]=='\0'){
+        //check kunta age shesh hoiche
+        if(a[i]=='\0'&&b[i]=='\0'){
             printf("Both are same");
             break;
         }
-        else if(a[i]='\0'){
+        else if(a[i]=='\0'){
             printf("A is smaller");
             break;
         }
@@ -19,16 +19,63 @@ int main(){
             printf("B is smaller");
             break;
         }
+        //check  different
         if(a[i]==b[i]){
             i++;
-        }else if(a[i]>b[i]){
+        }
+        else if(a[i]>b[i]){
             printf("B is smaller");
             break;
-        }
-        else{
+        }else{
             printf("A is smaller");
             break;
         }
     }
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main(){
+//     char a[100], b[100];
+//     scanf("%s %s",a,b);
+//     int i=0;
+//     while(1){
+//         //null checking
+//         if(a[i]=='\0'&& b[i]=='\0'){
+//             printf("Both are same");
+//             break;
+//         }
+//         else if(a[i]='\0'){
+//             printf("A is smaller");
+//             break;
+//         }
+//         else if(b[i]=='\0'){
+//             printf("B is smaller");
+//             break;
+//         }
+//         if(a[i]==b[i]){
+//             i++;
+//         }else if(a[i]>b[i]){
+//             printf("B is smaller");
+//             break;
+//         }
+//         else{
+//             printf("A is smaller");
+//             break;
+//         }
+//     }
+//     return 0;
+// }
